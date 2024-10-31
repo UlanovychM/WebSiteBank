6232e825733f1665105c465fcded1b9442ab5db4
Mykhailo <97468374+UlanovychM@users.noreply.github.com>
2024-10-31 19:36:07 +0200

    'update'

--- JS/modules/buttons/button.js
+++ JS/modules/buttons/button.js
@@ -2,14 +2,43 @@ const buttons = () => {
   const btnScroll = document.querySelector('.btn--scroll-to');
   const section = document.querySelector('#section--1');
 
+  //--------------New---------------
+  const navLinks = document.querySelector('.nav__links');
+
+  navLinks.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.classList.contains('nav__link')) {
+      const href = e.target.getAttribute('href');
+
+      document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    }
+    console.log(e.target);
+  });
+
+  //--------------Old---------------
+  // const navLinks = document.querySelectorAll('.nav__link');
+
+  // navLinks.forEach(nav =>
+  //   nav.addEventListener('click', e => {
+  //     e.preventDefault();
+
+  //     const href = nav.getAttribute('href');
+
+  //     document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+  //   })
+  // );
+
+  //-----------------------------
+
   btnScroll.addEventListener('click', e => {
+    //--------------Old---------------
     // const sectionClients = section.getBoundingClientRect();
     // window.scrollTo({
     //   left: sectionClients.left + window.scrollX,
     //   top: sectionClients.top + window.scrollY,
     //   behavior: 'smooth',
     // });
-
+    //--------------New---------------
     section.scrollIntoView({ behavior: 'smooth' });
   });
 };

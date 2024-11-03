889454662f815d7cd0fb8ca4c45b7c58268e8081
Mykhailo <97468374+UlanovychM@users.noreply.github.com>
2024-11-03 21:56:14 +0200

    'update'

--- JS/modules/navigation/navigation.js
+++ JS/modules/navigation/navigation.js
@@ -1,14 +1,34 @@
 import { section, nav } from '../buttons/button';
 
 const navigation = () => {
-  const section1 = section.getBoundingClientRect();
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > section1.top) {
+  //-------------OLD------------
+  // const section1 = section.getBoundingClientRect();
+  // window.addEventListener('scroll', () => {
+  //   if (window.scrollY > section1.top) {
+  //     nav.classList.add('sticky');
+  //   } else {
+  //     nav.classList.remove('sticky');
+  //   }
+  // });
+  //------------NEW----------------
+  const header = document.querySelector('.header');
+  const navHeight = nav.getBoundingClientRect().height;
+  const getStickyNav = entries => {
+    const entry = entries[0];
+
+    if (!entry.isIntersecting) {
       nav.classList.add('sticky');
     } else {
       nav.classList.remove('sticky');
     }
+  };
+
+  const observer = new IntersectionObserver(getStickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `${-navHeight}px`,
   });
+  observer.observe(header);
 };
 
 export default navigation;
